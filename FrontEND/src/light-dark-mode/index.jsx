3b507189697116c31f94c244d4d0ab532432eb0e
mushari44 <meeshoo-010@hotmail.com>
2024-04-26 06:48:05 +0300

    toggle added

--- FrontEND/src/light-dark-mode/index.jsx
+++ FrontEND/src/light-dark-mode/index.jsx
@@ -1,23 +1,34 @@
 import useLocalStorage from "./useLocalStorage";
 import { MdDarkMode } from "react-icons/md";
+import { IoToggleSharp } from "react-icons/io5";
+import { MdOutlineToggleOff } from "react-icons/md";
 import React from "react";
+import { useEffect } from "react";
 export default function LightDarkMode() {
   const [theme, setTheme] = useLocalStorage("them", "dark");
   function handleToggleTheme() {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
-    console.log("Setting theme to:", newTheme);
   }
   React.useEffect(() => {
     document.body.setAttribute("data-theme", theme);
   }, [theme]); // Re-run when theme changes
   return (
     <div className="light-dark-mode" data-theme={theme}>
-      <div className="container">
+      <div className="theme-container">
         <MdDarkMode
           onClick={handleToggleTheme}
           className="theme-mode"
         ></MdDarkMode>
+        {theme && theme === "dark" ? (
+          <IoToggleSharp onClick={handleToggleTheme} className="theme-mode-on" />
+        ) : (
+          <MdOutlineToggleOff
+            onClick={handleToggleTheme}
+            className="theme-mode-off"
+            s
+          />
+        )}
         <p>theme mode : {theme}</p>
       </div>
     </div>
